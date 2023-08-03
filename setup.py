fcd10226b47c37bab54a4bf09376be1d1f95ae5d
SanderBorgmans <sanderborgmans@gmail.com>
2023-08-03 13:39:52 +0200

    comma missing

--- setup.py
+++ setup.py
@@ -13,6 +13,6 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=['h5py>=2.0.0']
+    install_requires=['h5py>=2.0.0'],
     python_requires='>=3.6',
 )
